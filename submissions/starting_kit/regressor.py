370a771c47ece7efd352712f6f6c04c498d07ae3
Balazs Kegl <balazs.kegl@gmail.com>
2018-06-08 14:48:16 +0200

    more intuitive notebook

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -5,7 +5,7 @@ from sklearn.base import BaseEstimator
 class Regressor(BaseEstimator):
     def __init__(self):
         self.clf = RandomForestRegressor(
-            n_estimators=10, max_depth=10, max_features=10)
+            n_estimators=20, max_depth=50, max_features=10)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
