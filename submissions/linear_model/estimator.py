8d8cfefe19d89dd19b7076105d56b0c879450c20
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-01-24 16:34:09 +0100

    fix

--- submissions/linear_model/estimator.py
+++ submissions/linear_model/estimator.py
@@ -1,31 +1,10 @@
-import os
 import pandas as pd
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.compose import make_column_transformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import RandomForestRegressor
-
-
-def _merge_external_data(X):
-    filepath = os.path.join(
-        os.path.dirname(__file__), 'external_data.csv'
-    )
-    # Make sure that DateOfDeparture is of dtype datetime
-    X.loc[:, "DateOfDeparture"] = pd.to_datetime(X['DateOfDeparture'])
-    # Parse date to also be of dtype datetime
-    data_weather = pd.read_csv(filepath, parse_dates=["Date"])
-
-    X_weather = data_weather[['Date', 'AirPort', 'Events']]
-    X_weather = X_weather.rename(
-        columns={'Date': 'DateOfDeparture', 'AirPort': 'Arrival'}
-    )
-
-    X_merged = pd.merge(
-        X, X_weather, how='left', on=['DateOfDeparture', 'Arrival'], sort=False
-    )
-    return X_merged
+from sklearn.linear_model import LinearRegression
 
 
 def _encode_dates(X):
@@ -43,25 +22,22 @@ def _encode_dates(X):
 
 
 def get_estimator():
-    data_merger = FunctionTransformer(_merge_external_data)
-
     date_encoder = FunctionTransformer(_encode_dates)
-    date_cols = ["DateOfDeparture"]
 
-    categorical_encoder = make_pipeline(
-        SimpleImputer(strategy="constant", fill_value="missing"),
-        OrdinalEncoder()
-    )
-    categorical_cols = ['Arrival', 'Departure', 'Events']
+    categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+    categorical_cols = [
+        "Arrival", "Departure", "year", "month", "day",
+        "weekday", "week", "n_days"
+    ]
+
+    numerical_scaler = StandardScaler()
+    numerical_cols = ["WeeksToDeparture", "std_wtd"]
 
     preprocessor = make_column_transformer(
-        (date_encoder, date_cols),
         (categorical_encoder, categorical_cols),
-        remainder='passthrough',  # passthrough numerical columns as they are
+        (numerical_scaler, numerical_cols)
     )
 
-    regressor = RandomForestRegressor(
-        n_estimators=10, max_depth=10, max_features=10, n_jobs=4
-    )
+    regressor = LinearRegression()
 
-    return make_pipeline(data_merger, preprocessor, regressor)
+    return make_pipeline(date_encoder, preprocessor, regressor)
