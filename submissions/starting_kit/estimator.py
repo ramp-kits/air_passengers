94866ffc86d748a25dbd166e1b4c4617c4de9e93
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-01-24 11:30:52 +0100

    simplify starting-kit

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -7,23 +7,10 @@ from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor
 
 
-def _merge_external_data(X):
-    X.loc[:, 'DateOfDeparture'] = pd.to_datetime(X.loc[:, 'DateOfDeparture'])
-    filepath = os.path.join(
-        os.path.dirname(__file__), 'external_data.csv'
-    )
-    data_weather = pd.read_csv(filepath, parse_dates=["Date"])
-    X_weather = data_weather[['Date', 'AirPort', 'Max TemperatureC']]
-    X_weather = X_weather.rename(
-        columns={'Date': 'DateOfDeparture', 'AirPort': 'Arrival'}
-    )
-    X_merged = pd.merge(
-        X, X_weather, how='left', on=['DateOfDeparture', 'Arrival'], sort=False
-    )
-    return X_merged
-
-
 def _encode_dates(X):
+    # Make sure that DateOfDeparture is of dtype datetime
+    X.loc[:, "DateOfDeparture"] = pd.to_datetime(X['DateOfDeparture'])
+    # Encode the date information from the DateOfDeparture columns
     X.loc[:, 'year'] = X['DateOfDeparture'].dt.year
     X.loc[:, 'month'] = X['DateOfDeparture'].dt.month
     X.loc[:, 'day'] = X['DateOfDeparture'].dt.day
@@ -32,22 +19,25 @@ def _encode_dates(X):
     X.loc[:, 'n_days'] = X['DateOfDeparture'].apply(
         lambda date: (date - pd.to_datetime("1970-01-01")).days
     )
-    return X
+    # Finally we can drop the original columns from the dataframe
+    return X.drop(columns=["DateOfDeparture"])
 
 
 def get_estimator():
-    merge_transformer = FunctionTransformer(_merge_external_data)
-    date_transformer = FunctionTransformer(_encode_dates)
+    date_encoder = FunctionTransformer(_encode_dates)
+    date_cols = ["DateOfDeparture"]
 
-    categorical_cols = ['Arrival', 'Departure']
-    drop_col = ['DateOfDeparture']
+    categorical_encoder = OrdinalEncoder()
+    categorical_cols = ["Arrival", "Departure"]
+
     preprocessor = make_column_transformer(
-        (OrdinalEncoder(), categorical_cols),
-        ('drop', drop_col),
-        remainder='passthrough'
+        (date_encoder, date_cols),
+        (categorical_encoder, categorical_cols),
+        remainder='passthrough',  # passthrough numerical columns as they are
     )
-    pipeline = make_pipeline(
-        merge_transformer, date_transformer, preprocessor,
-        RandomForestRegressor(n_estimators=10, max_depth=10, max_features=10)
+
+    regressor = RandomForestRegressor(
+        n_estimators=10, max_depth=10, max_features=10, n_jobs=4
     )
-    return pipeline
+
+    return make_pipeline(preprocessor, regressor)
